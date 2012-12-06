3ecf602854771f594bc6bdbf218abdc919ebe116
Jay <jay@Jay-Linux.(none)>
2012-12-06 01:55:59 -0800

    request query handling

--- MemeShare/views.py
+++ MemeShare/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.template.loader import get_template
 from django.template import Context
 from core import *
