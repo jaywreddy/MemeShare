40fd0aaf7be9197005c6b19d1359592ccc59fc63
Jay <jay@Jay-Linux.(none)>
2012-12-07 03:20:20 -0800

    routing to backend

--- MemeShare/views.py
+++ MemeShare/views.py
@@ -11,6 +11,45 @@ def test(request):
     html=t.render(Context({}))
     return HttpResponse(html)
 
+def home(request):
+    html=get_template('index.html').render(Context({}))
+    return HttpResponse(html)
+
+def login(request):
+    if (request.POST and 'username' in request.POST):
+        user_id=request.POST['username']
+    else:
+        user_id=''
+    html=get_template('memegroups.html').render(Context({}))
+    response=HttpResponse(html)
+    response.set_cookie('uuid',user_id)
+    return response
+
+def route_add_user_to_memegroup(request):
+    user_id=request.GET('uuid') 
+    group_id=request.GET('group_id')
+    return HttpResponse(add_user_to_memegroup(user_id, group_id))  
+
+def route_create_memegroup(request):
+    uuid=request.COOKIES['uuid'] #cookie
+    name=request.GET['name']
+    group_id=create_memegroup(name)
+    add_user_to_mememgroup(uuid, group_id)
+    return HttpResponse('True')
+
+def route_get_memegroup_memes(request):
+    memegroup_id=request.GET['group_id']
+    memes=get_memegroup_memes(memegroup_id)
+    return HttpResponse(memes)
+
+def route_get_user_memegroups(request):
+    uuid=request.COOKIES['uuid']
+    memegroups=get_user_memegroups(uuid)
+    return HttpResponse(memegroups)
+
+
+
+
 
 def add_memegroup(request):    
     memegroup_name="floor 4 lol"
